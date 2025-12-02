f6a947a6697c3b357b33262457272f2529aa2501
horkypetr94 <horkypetr94@gmail.com>
2025-12-02 21:29:58 +0100

    add advent of code day 1 and day 2

---
+++ day_1.py
@@ -0,0 +1,31 @@
+from helpers import create_list_from_lines
+
+items = create_list_from_lines("data/day_1_1")
+
+safe_value = 50
+zeros_counter = 0
+zero_passes_counter = 0
+
+for item in items:
+    direction = item[0]
+    number = int(item[1:])
+
+    if direction == "R":
+        step = 1
+    elif direction == "L":
+        step = -1
+    else:
+        raise ValueError("direction must be 'R' or 'L'")
+
+    for _ in range(number):
+        # -10 % 100 = 90
+        safe_value = (safe_value + step) % 100
+        if safe_value == 0:
+            zero_passes_counter += 1
+
+    if safe_value % 100 == 0:
+        zeros_counter += 1
+
+
+print(f"Day 1_1 result is {zeros_counter}")
+print(f"Day 1_2 result is {zero_passes_counter}")
---
+++ day_2.py
@@ -0,0 +1,48 @@
+from helpers import create_str_from_line
+
+def task_1(product_ids: list[str]):
+    code_counter = 0
+    for product_id in product_ids:
+        start, end = product_id.split("-")
+        int_start = int(start)
+        int_end = int(end)
+
+        for code in range(int_start, int_end + 1):
+            str_code = str(code)
+            code_length = len(str_code)
+
+            if code_length % 2 == 0:
+                left_hand, right_hand = (
+                    str_code[: code_length // 2],
+                    str_code[code_length // 2 :],
+                )
+                if left_hand == right_hand:
+                    code_counter += code
+
+    print(f"The result 1 is: {code_counter}")
+
+
+def task_2(product_ids: list[str]):
+    code_counter = 0
+    for product_id in product_ids:
+        start, end = product_id.split("-")
+        int_start = int(start)
+        int_end = int(end)
+
+        for code in range(int_start, int_end + 1):
+            str_code = str(code)
+            for val in range(1, len(str_code) // 2 + 1):
+                code_splits = str_code.split(str_code[:val])
+                # find patterns by splitting
+                # e.g. "1188511885".split("11885") is ['', '', '']
+                if not any(code_splits):
+                    code_counter += code
+                    # exit loop to prevent from repeating as 2222 would be counted as 2 2 2 2 and 22 22
+                    break
+
+    print(f"The result 2 is: {code_counter}")
+
+items = create_str_from_line("data/day_2_1")
+product_ids = items.split(",")
+task_1(product_ids)
+task_2(product_ids)
