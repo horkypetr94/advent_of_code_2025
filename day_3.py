a8005e4e3efb6eea813d3baeff377e575b76b38f
horkypetr94 <horkypetr94@gmail.com>
2025-12-03 22:13:04 +0100

    refactor day_3

--- day_3.py
+++ day_3.py
@@ -3,28 +3,23 @@ from helpers import create_list_from_lines
 lines = create_list_from_lines("data/day_3_1")
 
 def find_highest_number_from_line(line: str, offset:int) -> tuple[int, int]:
-    highest_number = 0
-    highest_number_index = 0
+    digits = list(map(int, line))
+    highest_number = max(digits)
+    # Returns the index of the first element with the specified value
+    highest_rel_index = digits.index(highest_number)
 
-    for index, char in enumerate(line):
-        if int(char) > highest_number:
-            highest_number = int(char)
-            highest_number_index = index
-
-    return highest_number, highest_number_index + offset + 1
+    return highest_number, highest_rel_index + offset + 1
 
 
-def calculate_total_joltage(input_lines: list[str], num_cycles = int) -> int:
+def calculate_total_joltage(input_lines: list[str], num_cycles: int) -> int:
     joltage_counter = 0
     for input_line in input_lines:
         joltage_number_string = ""
         highest_index = -1
 
         for num_cycle in range(num_cycles-1, -1, -1 ):
-            if num_cycle > 0:
-                sub_line = input_line[highest_index + 1:-num_cycle]
-            else:
-                sub_line = input_line[highest_index + 1:]
+            end = -num_cycle if num_cycle > 0 else None
+            sub_line = input_line[highest_index + 1: end]
             highest, highest_index = find_highest_number_from_line(sub_line, highest_index)
             joltage_number_string += str(highest)
 
