2a5b6b7a465e21a457190352382e4a424309f4f6
horkypetr94 <horkypetr94@gmail.com>
2025-12-06 00:23:53 +0100

    add day 4

--- day_4.py
+++ day_4.py
@@ -1,44 +1,37 @@
-from helpers import load_list_of_lists, remap_lol_values, pad_lol_with_zeros
+from loaders import load_list_of_lists
+from tools import remap_lol_values, pad_lol_with_zeros, get_moore_neighbourhood
 
 array = load_list_of_lists("data/day_4_1")
-
-array = [
-    ['.', '.', '@', '@', '.', '@', '@', '@', '@', '.'],
-    ['@', '@', '@', '.', '@', '.', '@', '.', '@', '@'],
-    ['@', '@', '@', '@', '@', '.', '@', '.', '@', '@'],
-    ['@', '.', '@', '@', '@', '@', '.', '.', '@', '.'],
-    ['@', '@', '.', '@', '@', '@', '@', '.', '@', '@'],
-    ['.', '@', '@', '@', '@', '@', '@', '@', '.', '@'],
-    ['.', '@', '.', '@', '.', '@', '.', '@', '@', '@'],
-    ['@', '.', '@', '@', '@', '.', '@', '@', '@', '@'],
-    ['.', '@', '@', '@', '@', '@', '@', '@', '@', '.'],
-    ['@', '.', '@', '.', '@', '@', '@', '.', '@', '.']
-]
-array = remap_lol_values(array, {".":0, "@": 1})
+array = remap_lol_values(array, {".": 0, "@": 1})
 array = pad_lol_with_zeros(array)
 
-moore_neighbouhood_dict = {
-    "NW": (-1,-1),
-    "N": (-1,0),
-    "NE": (-1, 1),
-    "W": (0,-1),
-    "E": (0,1),
-    "SW": (1,-1),
-    "S": (1,0),
-    "SE": (1,1),
-}
-
-def get_moore_neighbourhood(line, item):
-    items = []
-    for diff in moore_neighbouhood_dict.values():
-        items.append(array[line+diff[0]][item+diff[1]])
-    return items
 
 global_counter = 0
-for i in range(1, len(array)-1):
-    for j in range(1, len(array[i])-1):
-        surroundings = get_moore_neighbourhood(i, j)
+for i in range(1, len(array) - 1):
+    for j in range(1, len(array[i]) - 1):
+        surroundings = get_moore_neighbourhood(array, i, j)
         if array[i][j] == 1 and sum(surroundings) < 4:
             global_counter += 1
 
 print(f"result 1: {global_counter}")
+
+
+global_counter = 0
+keep_iterating = True
+
+while keep_iterating:
+    new_array = array.copy()
+    local_counter = 0
+    for i in range(1, len(array) - 1):
+        for j in range(1, len(array[i]) - 1):
+            surroundings = get_moore_neighbourhood(array, i, j)
+            if array[i][j] == 1 and sum(surroundings) < 4:
+                local_counter += 1
+                new_array[i][j] = 0
+
+    global_counter += local_counter
+    array = new_array.copy()
+    if local_counter == 0:
+        keep_iterating = False
+
+print(f"result 2: {global_counter}")
