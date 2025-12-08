fea00f002f516d263fe695e2f25fa433fa756ba9
horkypetr94 <horky@spaceknow.com>
2025-12-08 21:29:57 +0100

    loaders: add load_list_from_lines_no_strip

--- loaders.py
+++ loaders.py
@@ -8,6 +8,16 @@ def load_list_from_lines(path: str) -> list:
     return items
 
 
+def load_list_from_lines_no_strip(path: str) -> list:
+    items = []
+    with open(path) as f:
+        data = f.readlines()
+        for line in data:
+            items.append(line)
+
+    return items
+
+
 def load_str_from_line(path: str) -> str:
     with open(path) as f:
         data = f.readlines()
