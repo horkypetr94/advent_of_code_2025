cc2cd8e107bb99c8e0123eb3f3874f335e695543
horkypetr94 <horky@spaceknow.com>
2025-12-23 10:54:41 +0100

    add day 11

--- day_11.py
+++ day_11.py
@@ -1,45 +1,54 @@
 from loaders import load_list_from_lines_no_strip
+from tools import reverse_lookup
 
-input = [
-    "aaa: you hhh",
-    "you: bbb ccc",
-    "bbb: ddd eee",
-    "ccc: ddd eee fff",
-    "ddd: ggg",
-    "eee: out",
-    "fff: out",
-    "ggg: out",
-    "hhh: ccc fff iii",
-    "iii: out"
-]
-# input = load_list_from_lines_no_strip("data/day_11_1")
+input = load_list_from_lines_no_strip("data/day_11_1")
+# input = [
+#     "aaa: you hhh",
+#     "you: bbb ccc",
+#     "bbb: ddd eee",
+#     "ccc: ddd eee fff",
+#     "ddd: ggg",
+#     "eee: out",
+#     "fff: out",
+#     "ggg: out",
+#     "hhh: ccc fff iii",
+#     "iii: out"
+# ]
+
 server_dict = {}
 
 for line in input:
     input_val, output_val = line.split(":")
     server_dict[input_val] = output_val.split()
 
-def keys_for_given_values(given_values: list) -> list:
-    current_keys = []
-    for given_value in given_values:
-        for key, line in server_dict.items():
-            if given_value in line:
-                current_keys.append(key)
-    return current_keys
+
+def keys_for_given_values(current_counts: dict) -> dict:
+    next_counts = {}
+    for value, count in current_counts.items():
+        if value in server_dict_lookup:
+            found_parents = server_dict_lookup[value]
+            for parent in found_parents:
+                next_counts[parent] = next_counts.get(parent, 0) + count
+
+    return next_counts
 
 
-all_valid_values = ["out"]
-current_keys = ["out"]
+server_dict_lookup = reverse_lookup(server_dict)
+current_keys = {"out": 1}
+yous = 0
+
+while len(current_keys) > 0:
+    current_keys = keys_for_given_values(current_keys)
+    if "you" in current_keys:
+        yous += current_keys["you"]
+
+print(f"result 1: {yous}")
+
+
+current_keys = {"svr": 1}
 yous = 0
 while len(current_keys) > 0:
     current_keys = keys_for_given_values(current_keys)
-    yous += current_keys.count("you")
-    all_valid_values.extend(current_keys)
-    all_valid_values = list(set(all_valid_values))
-print(yous)
-
-
-
-
-
-
+    if "you" in current_keys:
+        yous += current_keys["you"]
+print(f"result 2: {yous}")
